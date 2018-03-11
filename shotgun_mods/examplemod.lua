a318673dd59a017c04582c422e55cb860477d0a5
Alex <atenefyr@gmail.com>
2018-03-10 23:32:28 -0600

    adjust magical shell

--- shotgun_mods/examplemod.lua
+++ shotgun_mods/examplemod.lua
@@ -7,17 +7,39 @@
 modName = 'Example Mod'
 
 function ai_emptyshell(currentAmmo, playerAmmo, playersLastMove, botsLastMove, playerIsCursed, botIsCursed, playerHasSuccumbed, isPredicting, playersCurrentMove, seed, localValues)
+    math.randomseed(seed); math.random();
+    
     local botHasSuccumbed = localValues['succumbed']
     if botHasSuccumbed then -- the AI after the bot has succumbed
-        currentAmmo = currentAmmo - 2
         if currentAmmo <= 0 then
             return 92, '%botname% ran out of ammo.' -- force a lose if there is no more ammo
         end
-        return 6, {currentAmmo} -- play nothing and set the ammo
+        if playerAmmo <= 0 then
+            return 93, '%botname% sucked the life out of you.' -- force a win if you are leeched to death
+        end
+
+        return 3, {nil, playerAmmo-1}, localValues, 'Shoot & Leech', true, colours.red -- disguise shoot as 'shoot and leech' and take away ammo from player (we want to lose one ammo, so let the shoot take it away)
     else
-        if currentAmmo >= 6 then
+        if currentAmmo >= 8 then
             playSound('minecraft:entity.wither.spawn') -- play the wither spawn sound
             return 8, {currentAmmo - 2}, {['succumbed'] = true} -- play Succumb, subtract 2 ammo, mark it as having succumbed already
+        end
+
+        -- try to preserve self and accumulate ammo
+        if currentAmmo >= 1 then
+            if playerAmmo > 0 and not playerIsCursed then
+                if math.random(1,4) == 1 then
+                    return 2
+                elseif math.random(1,4) == 1 then
+                    return 4
+                end
+            end
+        else
+            if playerAmmo >= 1 and not playerIsCursed then
+                if math.random(1,3) == 1 then
+                    return 2
+                end
+            end
         end
     end
     return 1 -- reload if a move has not been decided yet
@@ -70,5 +92,5 @@ function ai_concealed(currentAmmo, playerAmmo, playersLastMove, botsLastMove, pl
     return botsCurrentMove, {}, {}, '???' -- hide what they are actually playing (but prophet can still see through)
 end
 
-aiList = {"Ninja", "Wizard", "Empty Shell", "Kitten", "Odium"}
-aiFunctions = {["Empty Shell"] = ai_emptyshell, ["Wizard"] = ai_mindcontrol, ["Ninja"] = ai_concealed, ["Odium"] = ai_winner, ["Kitten"] = ai_loser}
+aiList = {"Ninja", "Wizard", "Magical Shell", "Kitten", "Odium"}
+aiFunctions = {["Magical Shell"] = ai_emptyshell, ["Wizard"] = ai_mindcontrol, ["Ninja"] = ai_concealed, ["Odium"] = ai_winner, ["Kitten"] = ai_loser}
